d5c7f68b8568db58aed7c9d63c1e60bd368eaadc
Nikhil Upadhaya <nikhil.upadhaya001@gmail.com>
2025-04-13 17:00:23 -0400

    firebase test commit

--- app/src/main/java/edu/uga/cs/finalproject/LogInScreen.java
+++ app/src/main/java/edu/uga/cs/finalproject/LogInScreen.java
@@ -4,18 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class LogInScreen extends AppCompatActivity {
-
-
+    public static final String TAG = "finalProject";
+    private FirebaseAuth mAuth;
     Button loginButton;
     Button registerButton;
 
@@ -28,6 +43,45 @@ public class LogInScreen extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView textView = findViewById( R.id.textView );
+        mAuth = FirebaseAuth.getInstance();
+        String email = "dawg@mail.com";
+        String password = "password";
+
+        mAuth.signInWithEmailAndPassword( email, password )
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success
+                            Log.d( TAG, "signInWithEmail:success" );
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        }
+                        else {
+                            // If sign in fails
+                            Log.d( TAG, "signInWithEmail:failure", task.getException() );
+                        }
+                    }
+                });
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference( "message" );
+        // Read from the database value for ”message”
+        myRef.addValueEventListener( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once, initially, and when data is updated
+                String message = dataSnapshot.getValue(String.class);
+                Log.d( TAG, "Read message: " + message );
+                textView.setText( message );
+            }
+            @Override
+            public void onCancelled( DatabaseError error ) {
+                // Failed to read value
+                Log.d( TAG, "Failed to read value.", error.toException() );
+            }
         });
 
         // initialize UI elements
