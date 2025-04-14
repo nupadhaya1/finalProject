b41b261afd7d616cea3524aa4904bee65fd9f006
pp47735 <pp47735@uga.edu>
2025-04-14 11:59:01 -0400

    Started Connecting Fragments Together

--- app/src/main/java/edu/uga/cs/finalproject/MainScreen.java
+++ app/src/main/java/edu/uga/cs/finalproject/MainScreen.java
@@ -4,9 +4,9 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainScreen extends AppCompatActivity {
 
@@ -15,10 +15,43 @@ public class MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Set the default fragment
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainerView, new RideFragment())
+                .commit();
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            int itemId = item.getItemId();
+            if (itemId == R.id.nav_driver) {
+                selectedFragment = new RideFragment();
+            } else if (itemId == R.id.nav_customer) {
+                selectedFragment = new DriverFragment();
+            } else if (itemId == R.id.nav_profile) {
+                selectedFragment = new ProfileFragment();
+            } else if (itemId == R.id.nav_chart) {
+                selectedFragment = new HistoryFragment();
+            } else if (itemId == R.id.nav_about) {
+                selectedFragment = new AboutFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, selectedFragment)
+                        .commit();
+            }
+
+            return true;
         });
+
+
+
+
+
+
     }
 }
--- app/src/main/java/edu/uga/cs/finalproject/RegisterScreen.java
+++ app/src/main/java/edu/uga/cs/finalproject/RegisterScreen.java
@@ -1,12 +1,15 @@
 package edu.uga.cs.finalproject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Toast;
 
 public class RegisterScreen extends AppCompatActivity {
 
@@ -15,10 +18,21 @@ public class RegisterScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register_screen);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+
+        Button registerButton = findViewById(R.id.registerEscape);
+        registerButton.setOnClickListener(v -> {
+            Intent intent = new Intent(RegisterScreen.this, LogInScreen.class);
+            startActivity(intent);
+            Toast.makeText(this, "Registered successfully!", Toast.LENGTH_SHORT).show();
+
+            finish();
         });
     }
 }
